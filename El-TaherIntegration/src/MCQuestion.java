e96004bdb8f04073f8c80796338116b3d3280656
eltah <eltah@10.0.0.65>
2019-04-19 03:54:30 -0400

    Fixed formatting in MCQuestion.java

--- El-TaherIntegration/src/MCQuestion.java
+++ El-TaherIntegration/src/MCQuestion.java
@@ -3,68 +3,68 @@
 //In this case, this class contains the properties from the Question class.
 //These fields and methods are obtained with the extends keyword.
 public class MCQuestion extends Question {
-	private String questionText;
-	private String choiceA;
-	private String choiceB;
-	private String choiceC;
-	private String choiceD;
-	private String correctAnswer;
-	
-	
-	public String getQuestionText() {
-	    return questionText;
-	}
-	
-	public void setQuestionText(String newQuestionText)  {
-	    questionText = newQuestionText;
-	}
-	
-	public String getChoiceA() {
-		return choiceA;
-	}
+    private String questionText;
+    private String choiceA;
+    private String choiceB;
+    private String choiceC;
+    private String choiceD;
+    private String correctAnswer;
 
-	public void setChoiceA(String newChoiceA) {
-		choiceA = newChoiceA;
-	}
+    public String getQuestionText() {
+        return questionText;
+    }
 
-	public String getChoiceB() {
-		return choiceB;
-	}
+    public void setQuestionText(String newQuestionText) {
+        questionText = newQuestionText;
+    }
 
-	public void setChoiceB(String newChoiceB) {
-		choiceB = newChoiceB;
-	}
+    public String getChoiceA() {
+        return choiceA;
+    }
 
-	public String getChoiceC() {
-		return choiceC;
-	}
+    public void setChoiceA(String newChoiceA) {
+        choiceA = newChoiceA;
+    }
 
-	public void setChoiceC(String newChoiceC) {
-		choiceC = newChoiceC;
-	}
+    public String getChoiceB() {
+        return choiceB;
+    }
 
-	public String getChoiceD() {
-		return choiceD;
-	}
+    public void setChoiceB(String newChoiceB) {
+        choiceB = newChoiceB;
+    }
 
-	public void setChoiceD(String newChoiceD) {
-		choiceD = newChoiceD;
-	}
-	
-	public String getCorrectAnswer() {
-		return correctAnswer;
-	}
-	
-	public void setCorrectAnswer(String newCorrectAnswer) {
-		correctAnswer = newCorrectAnswer;
-	}
-	@Override
-	public void askQuestion() {
-	  super.askQuestion();
-	  System.out.println(questionText);
-	  System.out.println(choiceA);
-	  System.out.println(choiceB);
-	  System.out.println(choiceC);
-		System.out.println(choiceD);
-	}
+    public String getChoiceC() {
+        return choiceC;
+    }
+
+    public void setChoiceC(String newChoiceC) {
+        choiceC = newChoiceC;
+    }
+
+    public String getChoiceD() {
+        return choiceD;
+    }
+
+    public void setChoiceD(String newChoiceD) {
+        choiceD = newChoiceD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String newCorrectAnswer) {
+        correctAnswer = newCorrectAnswer;
+    }
+
+    @Override
+    public void askQuestion() {
+        super.askQuestion();
+        System.out.println(questionText);
+        System.out.println(choiceA);
+        System.out.println(choiceB);
+        System.out.println(choiceC);
+        System.out.println(choiceD);
+    }
 }
